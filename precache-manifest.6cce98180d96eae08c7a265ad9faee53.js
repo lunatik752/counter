7e776fc74eb00cffa0cf7d23f8d83d49b191c486
lunatik752 <whitemaxim752@gmail.com>
2020-04-25 14:07:33 +0300

    Updates

---
+++ precache-manifest.6cce98180d96eae08c7a265ad9faee53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9225e03065d77ea4b2e2fda806e7fa",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "eceeb7e7aadea57e44e0",
+    "url": "/counter/static/css/main.c8ce6172.chunk.css"
+  },
+  {
+    "revision": "0d82c2bcd8e867824dc1",
+    "url": "/counter/static/js/2.09defcad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.09defcad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eceeb7e7aadea57e44e0",
+    "url": "/counter/static/js/main.fa61d7ad.chunk.js"
+  },
+  {
+    "revision": "345446230396b9bddf62",
+    "url": "/counter/static/js/runtime-main.37c0ace9.js"
+  }
+]);
